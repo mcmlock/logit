d2daef13d4804d5238d4e3c8e5182309d70bc7cf
Miles Morlock <milesmorlockcode@gmail.com>
2022-02-16 16:53:32 -0500

    Can export data from a progress meter to an excel spread sheet

--- components/LogHistoryScreen.js
+++ components/LogHistoryScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, ScrollView, SafeAreaView, View, TouchableOpacity, Text } from 'react-native';
+import XLSX from 'xlsx';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+
 
 const LogHistoryScreen = props => {
     const navigation = useNavigation();
@@ -84,8 +88,26 @@ const LogHistoryScreen = props => {
             <View style={styles.buttonsView}>
                 <TouchableOpacity 
                 style={styles.button}
-                onPress={() => {
-
+                onPress={async () => {
+                    var data = logs;
+                    var ws = XLSX.utils.json_to_sheet(data);
+                    var wb = XLSX.utils.book_new();
+                    XLSX.utils.book_append_sheet(wb, ws, "Logs");
+                    const wbout = XLSX.write(wb, {
+                      type: 'base64',
+                      bookType: "xlsx"
+                    });
+                    const uri = FileSystem.cacheDirectory + 'logs.xlsx';
+                    console.log(`Writing to ${JSON.stringify(uri)} with text: ${wbout}`);
+                    await FileSystem.writeAsStringAsync(uri, wbout, {
+                      encoding: FileSystem.EncodingType.Base64
+                    });
+                    
+                    await Sharing.shareAsync(uri, {
+                      mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                      dialogTitle: 'Log data',
+                      UTI: 'com.microsoft.excel.xlsx'
+                    });
                 }}>
                     <Text style={styles.btnText}>Export</Text>
                 </TouchableOpacity>
